af3bfbb90f57d9b467803f56b6338d6d0b9a5a45
shubham chandge <115560962+shubhamchandge@users.noreply.github.com>
2023-01-17 20:10:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ let text = document.getElementById("evaluatedText");
 let letterCount = document.getElementById("letterCount");
 
 text.addEventListener("input", ()=>{
-    letterCount.innerHTML=text.value.length;
+   const textvalue = text.slice(" ")
+	letterCount.innerhtml= textvalue.value;
 })
 
-console.log("working");
+
